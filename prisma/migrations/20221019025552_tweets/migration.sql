b7413ebf6e52fa8431116ce18270a1e9c01c17ca
Denis Cheung <d.s.h.cheung@gmail.com>
2022-10-19 10:58:41 +0800

    update

---
+++ prisma/migrations/20221019025552_tweets/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Tweet" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "text" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
+);
