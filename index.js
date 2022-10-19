806bbb442ac8d64c6940bc83738f56c84515145a
Denis Cheung <d.s.h.cheung@gmail.com>
2022-10-19 10:11:24 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const morgan = require('morgan')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!', query: req.query })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
